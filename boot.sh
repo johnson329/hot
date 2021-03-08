e9f28796347c4c108763c3d95585e86a6297ad9d
johnson <you@example.com>
2021-03-09 00:28:07 +0800

    if docker env

--- boot.sh
+++ boot.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # docker with docker-compose
-curl -sSL https://get.daocloud.io/docker | sh
-curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+docker -v
+if [ $? -eq  0 ]; then
+  echo "检查到Docker已安装!"
+else
+  echo "安装docker环境..."
+  curl -sSL https://get.daocloud.io/docker | sh
+  curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  echo "安装docker环境...安装完成!"
+fi
 
 # clone project
 cd $HOME/hot

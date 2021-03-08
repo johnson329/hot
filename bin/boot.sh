200600103adbbe344e38220fc9d80b6b2ae95fc9
johnson <you@example.com>
2021-03-09 01:01:24 +0800

    boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -2,5 +2,11 @@
 cd $HOME/hot
 docker-compose up -d
 sudo apt install virtualenv
-virtualenv --python=python3 env
+ls $HOME/hot
+if [ $? -eq  0 ]; then
+  git pull
+  echo "项目存在"
+else
+  virtualenv --python=python3 env
+fi
 $HOME/hot/env/bin/pip3 install -r requirements.txt -i http://pypi.douban.com/simple/

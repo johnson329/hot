2c9c1a3775c00cdd44d00736b54a9ba108049a14
johnson <you@example.com>
2021-03-09 00:22:44 +0800

    update boot.sh

--- boot.sh
+++ boot.sh
@@ -9,7 +9,7 @@ cd $HOME/hot
 docker-compose up -d
 sudo apt install virtualenv
 virtualenv --python=python3 env
-$HOME/hot/env/bin/pip3 install -r requirements -i http://pypi.douban.com/simple/
+$HOME/hot/env/bin/pip3 install -r requirements.txt -i http://pypi.douban.com/simple/
 
 # crontab
 # https://www.cnblogs.com/lowmanisbusy/p/12048054.html

acef557e7eea211a32883681f351c9c0aa67e666
johnson <you@example.com>
2021-03-09 00:30:46 +0800

    mycron

--- boot.sh
+++ boot.sh
@@ -20,10 +20,6 @@ $HOME/hot/env/bin/pip3 install -r requirements.txt -i http://pypi.douban.com/sim
 
 # crontab
 # https://www.cnblogs.com/lowmanisbusy/p/12048054.html
-#导出crontab 任务
-sudo cat /var/spool/cron/crontabs/${USER} > crontabs.task
-# 追加crontab任务
-sudo cat mycron.task >> crontabs.task
 
-sudo crontab -u ${USER} crontabs.task
+sudo crontab -u ${USER} mycron.task
 
